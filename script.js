abfa7a5e27395e36530e58feffae3ea7ed5d1ee6
Yogndr <106022206+Yogndr@users.noreply.github.com>
2024-12-22 11:45:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function mincost(arr)
 { 
+	arr.sort();
+	let mincost=0;
+	let sum=0;
+	for(let i=0;i<n-1;i++){
+		mincost=mincost+arr[i]+arr[i+1];
+		sum=arr[i]+arr[i+1];
+		arr.push(sum);
+		arr.sort();
+	    }
+	return mincost;
+	
 //write your code here
 // return the min cost
   
